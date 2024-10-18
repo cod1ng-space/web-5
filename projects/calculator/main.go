d0ea2a8109e0138c2a89bb39ce305b130aa36b52
Daniil Voronin <danilka.voronin.05@gmail.com>
2024-10-18 16:43:49 +0300

    commit

--- projects/calculator/main.go
+++ projects/calculator/main.go
@@ -1,7 +1,34 @@
 package main
 
+import (
+	"fmt"
+)
+
 // реализовать calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+func calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		select {
+		case value := <-firstChan:
+			out <- value * value
+		case value := <-secondChan:
+			out <- value * 3
+		case <-stopChan:
+
+		}
+	}()
+	return out
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+	channel1, channel2 := make(chan int), make(chan int)
+	stopChan := make(chan struct{})
+	calc := calculator(channel1, channel2, stopChan)
+
+	// Тестирование трех случаев
+	channel1 <- 5
+	//channel2 <- 100
+	//stopChan <- struct{}{}
+	fmt.Println(<-calc)
 }
